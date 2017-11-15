47994161dbb49d72a366973ecafbb4effcde6760
James Clare <jamesclare@Jamess-MBP.home>
2017-11-15 17:40:39 -0500

    Add links to google maps

--- src/components/Business/Business.jsx
+++ src/components/Business/Business.jsx
@@ -11,9 +11,11 @@ class Business extends React.Component {
         <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{this.props.business.address}</p>
-            <p>{this.props.business.city}</p>
-            <p>{this.props.business.state} {this.props.business.zipCode}</p>
+            <a href={`https://www.google.com/maps/place/${this.props.business.name}+${this.props.business.address}`} target="_blank">
+              <p>{this.props.business.address}</p>
+              <p>{this.props.business.city}</p>
+              <p>{this.props.business.state} {this.props.business.zipCode}</p>
+            </a>
           </div>
           <div className="Business-reviews">
             <h3>{this.props.business.category}</h3>
